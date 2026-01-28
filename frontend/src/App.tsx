8e150a33b7a5908fef6a52f425083167b36d4fa2
yorunoken <me@yorunoken.com>
2026-01-29 01:44:18 +0300

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <main>
+            <h1>Vite + React</h1>
+            <div>
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p>Click on the Vite and React logos to learn more</p>
+        </main>
+    );
+}
+
+export default App;
