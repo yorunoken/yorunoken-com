72fb4e9a0a10a0528be67841b5ec17c972c49007
yorunoken <me@yorunoken.com>
2026-01-29 02:11:49 +0300

    backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::Router;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    let serve_dir = ServeDir::new("./frontend/dist")
+        .not_found_service(ServeFile::new("./frontend/dist/index.html"));
+
+    let app = Router::new().nest_service("/", serve_dir);
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("Listening on http://{}", addr);
+
+    // crash if unwrap fr
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
